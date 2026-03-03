bc90b97bbf35ff9f34b8c83fd312cee407fd9216
woheller69 <woheller69@t-online.de>
2026-03-03 10:08:19 +0100

    read stations from local .json

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import xbmcaddon
 import xbmcvfs
 import json
 import base64
+import os
 
 addonID = 'plugin.audio.radiobrowser'
 addon = xbmcaddon.Addon(id=addonID)
@@ -20,51 +21,22 @@ xbmcplugin.setContent(addon_handle, 'songs')
 
 my_stations = {}
 profile = xbmc.translatePath(addon.getAddonInfo('profile')).decode("utf-8")
-mystations_path = profile+'/mystations.json'
+mystations_path = profile + '/mystations.json'
+br_json_path = os.path.join(xbmc.translatePath(addon.getAddonInfo('path')).decode("utf-8"), 'br.json')
 
 import socket
 import random
 
-def get_radiobrowser_base_urls():
-    """
-    Get all base urls of all currently available radiobrowser servers
-
-    Returns: 
-    list: a list of strings
-
-    """
-    hosts = []
-    # get all hosts from DNS
-    ips = socket.getaddrinfo('all.api.radio-browser.info',
-                             80, 0, 0, socket.IPPROTO_TCP)
-    for ip_tupple in ips:
-        ip = ip_tupple[4][0]
-
-        # do a reverse lookup on every one of the ips to have a nice name for it
-        host_addr = socket.gethostbyaddr(ip)
-
-        # add the name to a list if not already in there
-        if host_addr[0] not in hosts:
-            hosts.append(host_addr[0])
-
-    # sort list of names
-    random.shuffle(hosts)
-    # add "https://" in front to make it an url
-    xbmc.log("Found hosts: " + ",".join(hosts))
-    return list(map(lambda x: "https://" + x, hosts))
-
 def LANGUAGE(id):
-    # return id
-    # return "undefined"
     return addon.getLocalizedString(id).encode('utf-8')
 
 def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
-def addLink(stationuuid, name, url, favicon, bitrate):
+def addLink(stationuuid, name, url, favicon, bitrate='128'):
     li = xbmcgui.ListItem(name, iconImage=favicon)
     li.setProperty('IsPlayable', 'true')
-    li.setInfo(type="Music", infoLabels={ "Title":name, "Size":bitrate})
+    li.setInfo(type="Music", infoLabels={"Title": name, "Size": bitrate})
     localUrl = build_url({'mode': 'play', 'stationuuid': stationuuid})
 
     if stationuuid in my_stations:
@@ -78,66 +50,20 @@ def addLink(stationuuid, name, url, favicon, bitrate):
 
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=False)
 
-def downloadFile(uri, param):
-    """
-    Download file with the correct headers set
-
-    Returns: 
-    a string result
-
-    """
-    paramEncoded = None
-    if param != None:
-        paramEncoded = json.dumps(param)
-        xbmc.log('Request to ' + uri + ' Params: ' + ','.join(param))
-    else:
-        xbmc.log('Request to ' + uri)
-
-    req = urllib2.Request(uri, paramEncoded)
-    req.add_header('User-Agent', 'KodiRadioBrowser/1.2.0')
-    req.add_header('Content-Type', 'application/json')
-    response = urllib2.urlopen(req)
-    data=response.read()
-
-    response.close()
-    return data
-
-def downloadApiFile(path, param):
-    """
-    Download file with relative url from a random api server.
-    Retry with other api servers if failed.
-
-    Returns: 
-    a string result
-
-    """
-    servers = get_radiobrowser_base_urls()
-    i = 0
-    for server_base in servers:
-        xbmc.log('Random server: ' + server_base + ' Try: ' + str(i))
-        uri = server_base + path
-
-        try:
-            data = downloadFile(uri, param)
-            return data
-        except Exception as e:
-            xbmc.log("Unable to download from api url: " + uri, xbmc.LOGERROR)
-            pass
-        i += 1
-    return {}
-
-def addPlayableLink(data):
-    dataDecoded = json.loads(data)
-    for station in dataDecoded:
-        addLink(station['stationuuid'], station['name'], station['url'], station['favicon'], station['bitrate'])
-
 def readFile(filepath):
-    with open(filepath, 'r') as read_file:
-        return json.load(read_file)
+    try:
+        with open(filepath, 'r') as read_file:
+            return json.load(read_file)
+    except Exception as e:
+        xbmc.log("Failed to read file: %s - %s" % (filepath, str(e)), xbmc.LOGERROR)
+        return []
 
 def writeFile(filepath, data):
-    with open(filepath, 'w') as write_file:
-        return json.dump(data, write_file)
+    try:
+        with open(filepath, 'w') as write_file:
+            json.dump(data, write_file)
+    except Exception as e:
+        xbmc.log("Failed to write file: %s - %s" % (filepath, str(e)), xbmc.LOGERROR)
 
 def addToMyStations(stationuuid, name, url, favicon, bitrate):
     my_stations[stationuuid] = {'stationuuid': stationuuid, 'name': name, 'url': url, 'bitrate': bitrate, 'favicon': favicon}
@@ -149,142 +75,118 @@ def delFromMyStations(stationuuid):
         writeFile(mystations_path, my_stations)
         xbmc.executebuiltin('Container.Refresh')
 
-# create storage
+# Ensure profiles exist
 if not xbmcvfs.exists(profile):
     xbmcvfs.mkdir(profile)
 
 if xbmcvfs.exists(mystations_path):
     my_stations = readFile(mystations_path)
 else:
-    writeFile(mystations_path, my_stations)
+    writeFile(mystations_path, {})
+
+# Load local stations once at startup (cache)
+stations_from_br = readFile(br_json_path)
 
 mode = args.get('mode', None)
 
 if mode is None:
-    localUrl = build_url({'mode': 'stations', 'url': '/json/stations/topclick/100'})
+    # Instead of fetching from radio-browser, show all stations from br.json
     li = xbmcgui.ListItem(LANGUAGE(32000), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'stations', 'url': '/json/stations/topvote/100'})
-    li = xbmcgui.ListItem(LANGUAGE(32001), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'stations', 'url': '/json/stations/lastchange/100'})
-    li = xbmcgui.ListItem(LANGUAGE(32002), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'stations', 'url': '/json/stations/lastclick/100'})
-    li = xbmcgui.ListItem(LANGUAGE(32003), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'tags'})
-    li = xbmcgui.ListItem(LANGUAGE(32004), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'countries'})
-    li = xbmcgui.ListItem(LANGUAGE(32005), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'search'})
-    li = xbmcgui.ListItem(LANGUAGE(32007), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    localUrl = build_url({'mode': 'mystations'})
-    li = xbmcgui.ListItem(LANGUAGE(32008), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
+    li.setProperty('Fanart_Image', 'special://home/addons/plugin.audio.radiobrowser/fanart.jpg')
+    xbmcplugin.addDirectoryItem(handle=addon_handle,
+                                url=build_url({'mode': 'stations', 'url': 'all'}),
+                                listitem=li,
+                                isFolder=True)
+    # Optionally: Add other static folders with no remote dependency
+    # E.g., filter by country or tag (if your br.json has countrycode etc.)
+    # For now, just one top-level list
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'tags':
-    data = downloadApiFile('/json/tags', None)
-    dataDecoded = json.loads(data)
-    for tag in dataDecoded:
-        tagName = tag['name']
-        if int(tag['stationcount']) > 1:
-            try:
-                localUrl = build_url({'mode': 'stations', 'key': 'tag', 'value' : base64.b32encode(tagName.encode('utf-8'))})
-                li = xbmcgui.ListItem(tagName, iconImage='DefaultFolder.png')
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-            except Exception as e:
-                xbmc.err(e)
-                pass
+    # Show tags from br.json (unique tags from all stations)
+    tag_set = set()
+    for s in stations_from_br:
+        if s.get('tags'):
+            tag_set.update(t.strip() for t in s['tags'].split(','))
+    for tag in sorted(tag_set):
+        localUrl = build_url({'mode': 'stations', 'key': 'tag', 'value': base64.b32encode(tag.encode('utf-8'))})
+        li = xbmcgui.ListItem(tag, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'countries':
-    data = downloadApiFile('/json/countries', None)
-    dataDecoded = json.loads(data)
-    for tag in dataDecoded:
-        countryName = tag['name']
-        if int(tag['stationcount']) > 1:
-            try:
-                localUrl = build_url({'mode': 'states', 'country': base64.b32encode(countryName.encode('utf-8'))})
-                li = xbmcgui.ListItem(countryName, iconImage='DefaultFolder.png')
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-            except Exception as e:
-                xbmc.log("Stationcount is not of type int", xbmc.LOGERROR)
-                pass
-
-    xbmcplugin.endOfDirectory(addon_handle)
-
-elif mode[0] == 'states':
-    country = args['country'][0]
-    country = base64.b32decode(country)
-    country = country.decode('utf-8')
-
-    data = downloadApiFile('/json/states/'+urllib.quote(country)+'/', None)
-    dataDecoded = json.loads(data)
-
-    localUrl = build_url({'mode': 'stations', 'key': 'country', 'value': base64.b32encode(country.encode('utf-8'))})
-    li = xbmcgui.ListItem(LANGUAGE(32006), iconImage='DefaultFolder.png')
-    xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-
-    for tag in dataDecoded:
-        stateName = tag['name']
-        if int(tag['stationcount']) > 1:
-            try:
-                localUrl = build_url({'mode': 'stations', 'key': 'state','value':base64.b32encode(stateName.encode('utf-8'))})
-                li = xbmcgui.ListItem(stateName, iconImage='DefaultFolder.png')
-                xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
-            except Exception as e:
-                xbmc.log("Stationcount is not of type int", xbmc.LOGERROR)
-                pass
+    # Show countries from br.json (DE, etc.)
+    country_set = set()
+    for s in stations_from_br:
+        if s.get('countrycode'):
+            country_set.add(s['countrycode'])
+    for country in sorted(country_set):
+        localUrl = build_url({'mode': 'stations', 'key': 'countrycode', 'value': base64.b32encode(country.encode('utf-8'))})
+        li = xbmcgui.ListItem(country, iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=localUrl, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'stations':
-    url = '/json/stations/search'
-    param = None
-    if 'url' in args:
-        url = args['url'][0]
-    else:
-        key = args['key'][0]
-        value = base64.b32decode(args['value'][0])
-        value = value.decode('utf-8')
-        param = dict({key:value})
-        param['order'] = 'clickcount'
-        param['reverse'] = True
+    # Handle filtering by tag, country, or show all
+    all_stations = stations_from_br
 
-    data = downloadApiFile(url, param)
-    addPlayableLink(data)
+    if 'key' in args:
+        key = args['key'][0]
+        value = base64.b32decode(args['value'][0]).decode('utf-8')
+
+        if key == 'tag':
+            # Filter by tag
+            filtered = []
+            for station in all_stations:
+                tags = station.get('tags', '')
+                if value.lower() in [t.strip().lower() for t in tags.split(',') if t]:
+                    filtered.append(station)
+            all_stations = filtered
+
+        elif key == 'countrycode':
+            all_stations = [s for s in all_stations if s.get('countrycode') == value]
+
+    # Now add them
+    for station in all_stations:
+        # Fallback bitrate if not present (optional)
+        bitrate = station.get('bitrate', '128')
+        addLink(
+            station['stationuuid'],
+            station['name'],
+            station['url'],
+            station.get('favicon', ''),
+            str(bitrate)
+        )
+
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'play':
     stationuuid = args['stationuuid'][0]
-    data = downloadApiFile('/json/url/' + str(stationuuid),None)
-    dataDecoded = json.loads(data)
-    uri = dataDecoded['url']
-    xbmcplugin.setResolvedUrl(addon_handle, True, xbmcgui.ListItem(path=uri))
+    # Look up station in local data (cached in stations_from_br)
+    target = None
+    for s in stations_from_br:
+        if s.get('stationuuid') == stationuuid:
+            target = s
+            break
+
+    if target:
+        uri = target['url']
+        xbmcplugin.setResolvedUrl(addon_handle, True, xbmcgui.ListItem(path=uri))
+    else:
+        xbmcplugin.setResolvedUrl(addon_handle, False, xbmcgui.ListItem())
 
 elif mode[0] == 'search':
     dialog = xbmcgui.Dialog()
     d = dialog.input(LANGUAGE(32011), type=xbmcgui.INPUT_ALPHANUM)
-
-    url = '/json/stations/byname/'+d
-    data = downloadApiFile(url, None)
-    addPlayableLink(data)
-
-    xbmcplugin.endOfDirectory(addon_handle)
+    if d:
+        filtered = [s for s in stations_from_br if d.lower() in s.get('name', '').lower()]
+        for station in filtered:
+            bitrate = station.get('bitrate', '128')
+            addLink(station['stationuuid'], station['name'], station['url'], station.get('favicon', ''), str(bitrate))
+        xbmcplugin.endOfDirectory(addon_handle)
 
 elif mode[0] == 'mystations':
     for station in my_stations.values():
